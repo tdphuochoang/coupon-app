7c84cd6114115df23a1f97ceec5f4dee070aa83a
Hoang Tran <tdll.trdphoang@gmail.com>
2023-05-16 11:50:34 -0700

    Installed Amplify Authentication and added configurations

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,12 @@ export default defineNuxtConfig({
 	modules: ["@nuxtjs/tailwindcss"],
 
 	css: ["@fortawesome/fontawesome-svg-core/styles.css"],
+	alias: {
+		"./runtimeConfig": "./runtimeConfig.browser",
+	},
+	vite: {
+		define: {
+			"window.global": {},
+		},
+	},
 });
