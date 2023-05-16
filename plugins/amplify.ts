9d7c75f4e0c22cf190d12e93d5bf4fd9a43cc76e
Hoang Tran <tdll.trdphoang@gmail.com>
2023-05-16 12:29:59 -0700

    Fixed name

--- plugins/amplify.ts
+++ plugins/amplify.ts
@@ -1,6 +1,6 @@
 import { defineNuxtPlugin } from "#app";
 import { Amplify, Auth } from "aws-amplify";
-import awsExports from "../src/aws-exports";
+import awsExports from "../src/aws-exports.js";
 
 export default defineNuxtPlugin((nuxtApp) => {
 	Amplify.configure(awsExports);
